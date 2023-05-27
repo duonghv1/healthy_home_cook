d6ac959e4284b7e7514d8498601fc0c9ee66c367
YRTang <SarahTYR09@outlook.com>
2023-05-27 10:59:12 -0700

    Update main.py getRecipeInstr() function

--- data-servers/main.py
+++ data-servers/main.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 # number of result (by default): 5
 
 min_amount = 50
-num_recipe = 5
+num_recipe = 10
 
 
-@app.route("/data")
+@app.route("/getReceipes")
 def get_recipes():
     # Retrieve multiple query parameters from the request URL
     # Types of n1, n2, n3 -> String
@@ -60,6 +60,51 @@ def get_recipes():
     return json_data
     # return render_template("recipes.html", recipes=info["results"])
 
+@app.route("/getRecipeInstr")
+def get_recipe_instr():
+    # Sample URL: http://127.0.0.1:5000/getRecipeInstr?id=324694
+    # Retrieve multiple query parameters from the request URL
+    # Types of n1, n2, n3 -> String
+    rid = request.args.get("id")
+
+    url = f"https://api.spoonacular.com/recipes/{rid}/analyzedInstructions?apiKey={os.environ.get('Spoon_api_key')}"
+    print(url)
+    print("api key:", os.environ.get("Spoon_api_key"))
+    
+    # response = urllib.request.urlopen(url)
+    response = requests.get(url)
+
+    # Check if the request was successful (HTTP status code 200)
+    if response.status_code == 200:
+        # Access the JSON data from the response
+        json_data = response.json()
+        # Process the JSON data as needed
+    else:
+        # Print the error message if the request was unsuccessful
+        print("Error:", response.status_code)
+
+    for d in json_data:
+        del d["name"]
+
+    #type(data)=list
+    #len(data)=2
+    result = []
+    for d in json_data:
+        di=d["steps"]
+        parts = []
+        for dii in di:
+            step = dict()
+            # dii -> dictionary; keys: equipment, ingredients, number, step
+            # we only keep number and step
+            # print(dii["step"])
+            step["number"] = dii["number"]
+            step["step"] = dii["step"]
+            parts.append(step)
+        # print()
+        result.append(parts)
+
+    return result
+    # return render_template("recipes.html", recipes=info["results"])
     
 if __name__ == '__main__':
     app.run(debug=True)
